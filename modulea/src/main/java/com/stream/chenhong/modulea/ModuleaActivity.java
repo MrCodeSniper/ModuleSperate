7cc7f4f34afd644dc756574963a1377e41a8d79e
chenhong <15168264355@163.com>
2018-10-21 23:52:41 +0800

    Initial commit

---
+++ modulea/src/main/java/com/stream/chenhong/modulea/ModuleaActivity.java
@@ -0,0 +1,18 @@
+package com.stream.chenhong.modulea;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+
+/**
+ * Created by Administrator on 2018\10\21 0021.
+ */
+
+public class ModuleaActivity extends Activity {
+
+    @Override
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.module_a_activity);
+    }
+}
